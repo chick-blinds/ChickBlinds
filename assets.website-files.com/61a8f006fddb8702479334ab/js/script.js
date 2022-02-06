303b648b42a108aa74753ff42e4cdad39089fe74
Potato-29 <csaprayas@gmail.com>
2022-02-06 12:24:36 +0530

    first commit

---
+++ assets.website-files.com/61a8f006fddb8702479334ab/js/script.js
@@ -0,0 +1,25 @@
+
+    var myImg = document.getElementById("logo");
+    var body = document.getElementById("body");
+    var button = document.getElementById("colorButton");
+
+
+    // button.addEventListener("click", function(){
+    // if (button.clicked = true){
+    // myImg.src = "/assets.website-files.com/61a8f006fddb8754149334dd/chickBlindLogo.png"     
+    // } else {
+    // myImg.src = "/assets.website-files.com/61a8f006fddb8754149334dd/chickBlindLogoWhite.png"   
+    // }
+    // });
+
+
+
+
+
+// if(button){
+//     myImg.src = "/assets.website-files.com/61a8f006fddb8754149334dd/chickBlindLogo.png"
+// }
+
+// else{
+//     myImg.src = "/assets.website-files.com/61a8f006fddb8754149334dd/chickBlindLogoWhite.png"
+// }
